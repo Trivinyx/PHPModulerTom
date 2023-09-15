31dc709f00c9004512269b59050786492be83a44
Trivinyx <tom.a.s.myre@gmail.com>
2023-09-15 20:13:21 +0200

    phpModule 2 half

---
+++ Module2/module2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../Styles/style.css">
+    <title>Module2</title>
+</head>
+<body>
+    <div id="HeaderBar">
+        <div id="homebutton">
+            <a class="navButton" href="../index.php">Home</a>
+        </div>
+        <div id="Title">
+            <h1>Module 2</h1>
+        </div>
+    </div>
+    <div class="mainContainer">
+        
+            <h2>Oppgave Liste</h2>
+
+        
+        <div class="listContainer">
+            <ul class="taskList">
+                <li><a class="button" href="task1.php">Oppgave 1</a></li>
+                <li><a class="button" href="task2.php">Oppgave 2</a></li>
+                <li><a class="button" href="task3.php">Oppgave 3</a></li>
+                <li><a class="button" href="task4.php">Oppgave 4</a></li>
+                <li><a class="button" href="task5.php">Oppgave 5</a></li>
+            </ul>
+        </div>
+    </div>
---
+++ Module2/task1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../Styles/style.css">
+    <link rel="stylesheet" type="text/css" href="../Styles/module2.css">
+    <title>Module1</title>
+</head>
+<body>
+    <div id="HeaderBar">
+        <div id="homebutton">
+            <a class="navButton" href="../index.php">Home</a>
+            <a class="navButton" href="module2.php">Module 2 Hub</a>
+        </div>
+        <div id="Title">
+            <h1>Module2</h1>
+        </div>
+    </div>
+    <div class="mainContainer">
+        <div id="titleBar">
+            <div id="previous">
+                <a class="button" href="">forrige</a>
+            </div>
+            <div class="title">
+                <h2>Oppgave 1</h2>
+            </div>
+            <div id="next">
+                <a class="button" href="task2.php">neste</a>
+            </div>
+        </div>
+    </div>
+</body>
